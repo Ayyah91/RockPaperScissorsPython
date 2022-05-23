4f807112f9571390eb25248a2cde69081a927bbc
Ayyah Albeeju <105661914+Ayyah91@users.noreply.github.com>
2022-05-23 17:58:29 +0200

    first commit

---
+++ Rock_Paper_Scissors.py
@@ -0,0 +1,41 @@
+import random
+
+#first of all we make a variable for user score and the computer 
+user_wins = 0
+computer_wins = 0
+
+# The list to specified the options for the user 
+options = ["rock", "paper", "scissors"]
+
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower() # we use lower() function to over come any change in the user input and to be in the safe side :)
+    if user_input == "q":
+        break
+
+    if user_input not in options:
+        continue
+
+    random_number = random.randint(0, 2)
+    # the index for every option>> rock: 0, paper: 1, scissors: 2
+    computer_pick = options[random_number]
+    print("Computer picked", computer_pick + ".")
+
+    if user_input == "rock" and computer_pick == "scissors":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You won!")
+        user_wins += 1
+
+    else:
+        print("You lost!")
+        computer_wins += 1
+
+print("You won", user_wins, "times.")
+print("The computer won", computer_wins, "times.")
+print("Goodbye!")
